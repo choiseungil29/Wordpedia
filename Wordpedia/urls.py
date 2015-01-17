6d619a3b0cb00f4804001795eb5e0f1536f896cb
particp <dnsl1928@naver.com>
2015-01-17 18:02:26 +0900

    최초 커밋 microsoft translator api 붙임

---
+++ Wordpedia/urls.py
@@ -0,0 +1,36 @@
+from flask import Flask
+from flask import render_template, request
+
+from microsofttranslator import Translator
+
+from Wordpedia import app
+from Wordpedia import mysql
+
+import json
+import urllib
+
+translator = Translator('Wordpedia', 'f1wB2fFCQMVoKPTFqPxMwO79Qxg816xYE7Y5eNF4lBk=')
+
+@app.route('/')
+def helloworld():
+	return 'Hello World!'
+
+#@app.route('/translate')
+#def translate():
+#	return translator.translate('hello', 'ko')
+
+@app.route('/translate/<path:toLang>', methods=['GET'])
+def translates(toLang):
+	resultDic = {}
+	for fromText in request.values.getlist('w'):
+		resultDic[fromText] = translator.translate(fromText, toLang, translator.detect_language(fromText))
+	return json.dumps(resultDic, ensure_ascii=False)
+
+@app.route('/test')
+def test():
+	return translator.detect_language('hi')
+
+
+'''Translate Language Code
+http://msdn.microsoft.com/en-us/library/hh456380.aspx
+'''
---
+++ run.py
@@ -0,0 +1,6 @@
+from Wordpedia import app
+
+import Wordpedia.urls
+
+if __name__ == '__main__':
+    app.run()
