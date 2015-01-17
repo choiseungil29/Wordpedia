65680568adeb9a7f474bf4dc142c4f9233d8ab21
particp <dnsl1928@naver.com>
2015-01-17 20:33:13 +0900

    add error handler

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
-import os
 from Wordpedia import app
 
 import Wordpedia.urls
 
+import os
+import sys
+import logging
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 if __name__ == '__main__':
     app.run()
