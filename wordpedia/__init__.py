273b19d7f7a896a86890a6d4a23ceda86bef7572
particp <choiseungil29@gmail.com>
2015-03-08 08:04:33 +0900

    connect to heroku db

--- wordpedia/__init__.py
+++ wordpedia/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clogic:tmddlf12@localhost/wordpedia"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fliapubcocxlhn:a5a8YjMKgeHEOFq8RHoBMUU6uC@ec2-107-20-244-39.compute-1.amazonaws.com:5432/daj8pkrf41q6hr"
 db = SQLAlchemy(app)
 
 from wordpedia.model.comment import Comment
