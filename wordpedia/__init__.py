355b8bdd124a8435c852d996ca91b923c1f54cfe
particp <choiseungil29@gmail.com>
2015-03-09 08:45:16 +0900

    서버와 연결

--- wordpedia/__init__.py
+++ wordpedia/__init__.py
@@ -4,8 +4,8 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clogic:tmddlf12@localhost/wordpedia"
-#app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fliapubcocxlhn:a5a8YjMKgeHEOFq8RHoBMUU6uC@ec2-107-20-244-39.compute-1.amazonaws.com:5432/daj8pkrf41q6hr"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://clogic:tmddlf12@localhost/wordpedia"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://fliapubcocxlhn:a5a8YjMKgeHEOFq8RHoBMUU6uC@ec2-107-20-244-39.compute-1.amazonaws.com:5432/daj8pkrf41q6hr"
 db = SQLAlchemy(app)
 
 from wordpedia.model.comment import Comment
