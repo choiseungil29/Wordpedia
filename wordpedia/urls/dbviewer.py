cfeb78d9b242d52cde579b0fa225c699b46db731
particp <choiseungil29@gmail.com>
2015-03-12 05:29:08 +0900

    remove dbviewer.py

--- wordpedia/urls/dbviewer.py
+++ wordpedia/urls/dbviewer.py
@@ -14,46 +14,6 @@ from wordpedia.model.collection import Collection
 
 import json
 
-@app.route('/get/collection', methods=['POST', 'GET'])
-def collection():
-	id = request.args['collectionId']
-
-	collection = session.query(Collection).filter_by(id=id).first()
-	if collection is None:
-		return '존재하지 않는 단어장입니다'
-
-	resumt = setCollection(collection)
-
-	return json.dumps(result, ensure_ascii=False)
-
-@app.route('/get/collection/all', methods=['POST', 'GET'])
-def collections():
-	result = []
-
-	for collection in session.query(Collection).all():
-		item = setCollection(collection)
-		result.append(item)
-
-	return json.dumps(result, ensure_ascii=False)
-
-@app.route('/get/collection/user', methods=['POST', 'GET'])
-def collectionsOfUser():
-	token = request.headers['token']
-	result = {}
-
-	user = session.query(User).filter_by(token=token).first()
-	if user is None:
-		return '존재하지 않는 유저입니다'
-
-	result['id'] = user.id
-	result['userId'] = user.userId
-	result['token'] = user.token
-	result['collections'] = []
-	for collection in user.collections.all():
-		item = setCollection(collection)
-		result['collections'].append(item)
-
-	return json.dumps(result, ensure_ascii=False)
 
 def setCollection(collection):
 	result = {}
